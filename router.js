71830a5f49261d155444d488434c109eafd4c1ce
Yutian Lin <YutianLin@hi-ti.com>
2016-07-13 14:56:05 +0800

    修改測試

--- router.js
+++ router.js
@@ -13,9 +13,10 @@ exports.route = route;
 function route(handle, pathname) {
   console.log("About to route a request for " + pathname);
   if (typeof handle[pathname] === 'function') {
-    handle[pathname]();
+    return handle[pathname]();
   } else {
     console.log("No request handler found for " + pathname);
+    return "404 Not found";
   }
 }
 
