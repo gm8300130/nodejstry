052e6336f556d62ec8d07f78148e69183ae9b0bc
Yutian Lin <YutianLin@hi-ti.com>
2016-07-13 14:31:44 +0800

    git測試

---
+++ index.js
@@ -0,0 +1,40 @@
+/* 基本載入 Server http 模組
+var server = require("./server");
+
+server.start();
+*/
+
+/**
+                            url.parse(string).query
+                                           |
+           url.parse(string).pathname      |
+                       |                   |
+                       |                   |
+                     ------ -------------------
+http://localhost:8888/start?foo=bar&hello=world
+                                ---       -----
+                                 |          |
+                                 |          |
+              querystring(string)["foo"]    |
+                                            |
+                         querystring(string)["hello"]
+**/
+/*
+//路由
+var server = require("./server");
+var router = require("./router");
+
+server.start(router.route);
+*/
+
+
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
